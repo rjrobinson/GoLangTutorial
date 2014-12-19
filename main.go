17b896f2abb16614ef85560d8ed5e610dece43bf
RJ Robinson <robertj.robinson@gmail.com>
2014-12-19 12:23:24 -0500

    goroutines explained

--- main.go
+++ main.go
@@ -17,21 +17,46 @@
 // 	}
 // }
 
+// package main
+//
+// import "fmt"
+//
+// func main() {
+// 	// defines the var a
+// 	a := 5
+// 	fmt.Println(a)
+//
+// 	// sets a different value to a
+// 	a = 10
+// 	fmt.Println(a)
+//
+// 	//another way to define a variable
+// 	var b int
+// 	b = 15
+// 	fmt.Println(b)
+// }
+
+// package main
+//
+// import "fmt"
+//
+// func main() {
+// 	/// this creates a slice of integers with len of 15
+// 	mySlice := make([]int, 15)
+// }
+
 package main
 
 import "fmt"
 
+func wait() {
+	//wait around with a forever loop
+	for {
+
+	}
+}
+
 func main() {
-	// defines the var a
-	a := 5
-	fmt.Println(a)
-
-	// sets a different value to a
-	a = 10
-	fmt.Println(a)
-
-	//another way to define a variable
-	var b int
-	b = 15
-	fmt.Println(b)
+	go wait()
+	fmt.Println("We didn't wait because it was called as a goroutine!")
 }
