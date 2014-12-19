85195d07dd0e39c425d7e32fb6aafd8f5a9f8d7b
RJ Robinson <robertj.robinson@gmail.com>
2014-12-19 12:35:27 -0500

    more goroutines

--- main.go
+++ main.go
@@ -45,18 +45,50 @@
 // 	mySlice := make([]int, 15)
 // }
 
-package main
+// package main
+//
+// import "fmt"
+//
+// func wait() {
+// 	//wait around with a forever loop
+// 	for {
+//
+// 	}
+// }
+//
+// func main() {
+// 	go wait()
+// 	fmt.Println("We didn't wait because it was called as a goroutine!")
+// }
 
-import "fmt"
-
-func wait() {
-	//wait around with a forever loop
-	for {
-
-	}
-}
-
-func main() {
-	go wait()
-	fmt.Println("We didn't wait because it was called as a goroutine!")
-}
+// package main
+//
+// import (
+// 	"fmt"
+// 	"net"
+// )
+//
+// //notice that in the arguments, the name of
+// //the variable comes first, then comes the
+// //type of the variable, just like in "var"
+// //declarations
+//
+// func manageClient(conn net.Conn) {
+// 	conn.Write([]byte("Hi!"))
+// 	conn.Close()
+// 	//do something with the client
+// }
+//
+// func main() {
+// 	//we are creating a server here that listens
+// 	//on port 1337. Notice that, similar to Ruby,
+// 	//a method can have two return values (although
+// 	//in Ruby, this would be an array instead)
+//
+// 	listener, err := net.Listen("tcp", ":1337")
+// 	for {
+// 		//accept a connection
+// 		connection, _ := listener.Accept()
+// 		go manageClient(connection)
+// 	}
+// }
