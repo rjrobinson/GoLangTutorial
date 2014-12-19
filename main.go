75cfbcd5cce7a4d7170893198ec5cfeaed908dc6
RJ Robinson <robertj.robinson@gmail.com>
2014-12-19 12:14:04 -0500

    declaring vars

--- main.go
+++ main.go
@@ -6,13 +6,32 @@
 // 	fmt.Println("Hello, World!")
 // }
 
+// package main
+//
+// import "fmt"
+//
+// func main() {
+// 	//the basic for loop
+// 	for i := 1; i < 100; i++ {
+// 		fmt.Println(i)
+// 	}
+// }
+
 package main
 
 import "fmt"
 
 func main() {
-	//the basic for loop
-	for i := 1; i < 100; i++ {
-		fmt.Println(i)
-	}
+	// defines the var a
+	a := 5
+	fmt.Println(a)
+
+	// sets a different value to a
+	a = 10
+	fmt.Println(a)
+
+	//another way to define a variable
+	var b int
+	b = 15
+	fmt.Println(b)
 }
