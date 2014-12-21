ebb8df9d82afdb787e4e692116427a44de44f74d
RJ Robinson <robertj.robinson@gmail.com>
2014-12-20 21:27:01 -0500

    added main2.go file for second ruby to go

--- main2.go
+++ main2.go
@@ -1,12 +1,32 @@
+// package main
+//
+// import "fmt"
+//
+// func main() {
+// 	fmt.Println("Hello world! . Again... we printed it again. ")
+//
+// 	// in ruby
+// 	// puts "Hello World"
+// 	// or
+// 	// p "Hello World"
+// }
+
 package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello world! . Again... we printed it again. ")
+func fibonacci() func() int {
+	x := 0
+	y := 1
+	return func() int {
+		x, y = y, x+y
+		return x
+	}
+}
 
-	// in ruby
-	// puts "Hello World"
-	// or
-	// p "Hello World"
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
 }
